c21c7a27d4c2fdf9b2d18f5c21f805464aad2f82
Vincent Talloir <clouarddista@gmail.com>
2019-05-20 11:59:47 +0200

    fix set name

--- class/pokemon.js
+++ class/pokemon.js
@@ -1,10 +1,10 @@
 class Pokemon {
 	constructor(name) {
-		this.name = name || "";
-	}
-	set name (name) {
-		this.name = name.charAt(0).toUpperCase() 
+		this._name = name.charAt(0).toUpperCase() 
 		+ name.slice(1).toLowerCase();
+	}
+	set name(newName) {
+		this._name = newName;
 	}
 	get name () {
 		return this.name;
